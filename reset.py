7b69af3170d4d54bb59c8ccac8c87044ce0a01bd
Hippyjim <hippyjim@gmail.com>
2015-12-05 15:42:48 +0000

    reset.py resets the snowpi by turning off all the LEDs

---
+++ reset.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+# Import required libraries
+import RPi.GPIO as GPIO
+
+# Tell GPIO library to use GPIO references
+GPIO.setmode(GPIO.BCM)
+
+LEDs = [7,8,9,22,18,17,23,24,25]
+# Setup LED pins as outputs
+for x in range(8):
+    GPIO.setup(LEDs[x], GPIO.OUT)
+    GPIO.output(LEDs[x], False)
+# Light white and blue LEDs
+for x in range(8):
+    GPIO.output(LEDs[x], False)
+
+# Reset GPIO settings
+GPIO.cleanup()
