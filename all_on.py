f52b6fb021c06c8f764ff05af0e52ccc59ad9982
Hippyjim <hippyjim@gmail.com>
2015-12-05 16:09:03 +0000

    all_on.py turns on all the LEDs and cleans up when it's done. reset doesn't display warnings if we didn't clean up properly

--- all_on.py
+++ all_on.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python
-
 # Import required libraries
 import RPi.GPIO as GPIO
+import time
 
 # Tell GPIO library to use GPIO references
 GPIO.setmode(GPIO.BCM)
 
 LEDs = [7,8,9,22,18,17,23,24,25]
 # Setup LED pins as outputs
-for x in range(8):
+for x in range(9):
     GPIO.setup(LEDs[x], GPIO.OUT)
     GPIO.output(LEDs[x], False)
 # Light all LEDs
-for x in range(8):
+for x in range(9):
     GPIO.output(LEDs[x], True)
 
-# Reset GPIO settings
-GPIO.cleanup()
+print("Press CTRL-C to exit")
+
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("\nExiting")
+    GPIO.cleanup()
--- reset.py
+++ reset.py
@@ -6,13 +6,16 @@ import RPi.GPIO as GPIO
 # Tell GPIO library to use GPIO references
 GPIO.setmode(GPIO.BCM)
 
+# Don't display a warning if we didn't clean up properly in our last script
+GPIO.setwarnings(False)
+
 LEDs = [7,8,9,22,18,17,23,24,25]
 # Setup LED pins as outputs
-for x in range(8):
+for x in range(9):
     GPIO.setup(LEDs[x], GPIO.OUT)
     GPIO.output(LEDs[x], False)
 # Turn off all LEDs
-for x in range(8):
+for x in range(9):
     GPIO.output(LEDs[x], False)
 
 # Reset GPIO settings
